f9af1eb435aec24ecc26230929c6580c6826c4b6
budhash <budhash@gmail.com>
2023-06-05 07:37:33 -0700

    initial commit

--- ServoDriver.py
+++ ServoDriver.py
@@ -1,115 +1,236 @@
-# Pico Kitronik Simply Servos 
-# Uses the PIO state machines to drive a servo.
-# This is the Micro Python version. 
+# Pico Servo Driver
+# ServoDriver: uses the PIO state machines to drive a servo
 from machine import Pin
 from rp2 import PIO, StateMachine, asm_pio
 from time import sleep
 
-# List of which StateMachines we have used
-usedSM = [False, False, False, False, False, False, False, False]
+
+class InternalServoDriverException(Exception):
+    pass
+
+class UnregisteredServoException(Exception):
+    pass
+
+class InvalidServoSettingException(Exception):
+    pass
 
 class ServoDriver:
-    #Servo 0 degrees -> pulse of 0.5ms, 180 degrees 2.5ms
-    #pulse train freq 50hz - 20mS
-    #1uS is freq of 1000000
-    #servo pulses range from 500 to 2500usec and overall pulse train is 20000usec repeat.
-    #servo pins on the Simply Servos board are: GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9 for servos 1-8 in order
-    maxServoPulse = 2500
-    minServoPulse = 500
-    pulseTrain = 20000
-    degreesToUS = 2000/180
+    """
+    Driver class used to control servos using the PIO state machines
+    """
+    # servo : 0 degrees -> pulse of 0.5ms, 180 degrees 2.5ms
+    # pulse train freq 50hz - 20mS
+    # 1uS is freq of 1000000
+    # servo pulses range from 500 to 2500usec and overall pulse train is 20000usec repeat
+    MIN_SERVO_PULSE = 500
+    MAX_SERVO_PULSE = 2500
+    PULSE_TRAIN = 20000
+    DEGREES_TO_PULSE_LEN = 2000/180
     
-    #this code drives a pwm on the PIO. It is running at 2Mhz, which gives the PWM a 1uS resolution. 
+    # this code drives a pwm on the PIO. It is running at 2Mhz, which gives the PWM a 1uS resolution. 
     @asm_pio(sideset_init=PIO.OUT_LOW)
     def _servo_pwm():
-    #first we clear the pin to zero, then load the registers. Y is always 20000 - 20uS, x is the pulse 'on' length.     
+        """
+        Private method to generate the pulse width modulation signal required by the servo
+        """
+        # first we clear the pin to zero, then load the registers. Y is always 20000 - 20uS, x is the pulse 'on' length.     
         pull(noblock) .side(0)
-        mov(x, osr) # Keep most recent pull data stashed in X, for recycling by noblock
+        mov(x, osr) # keep most recent pull data stashed in X, for recycling by noblock
         mov(y, isr) # ISR must be preloaded with PWM count max
-    #This is where the looping work is done. the overall loop rate is 1Mhz (clock is 2Mhz - we have 2 instructions to do)    
+        # this is where the looping work is done. the overall loop rate is 1Mhz (clock is 2Mhz - we have 2 instructions to do)    
         label("loop")
-        jmp(x_not_y, "skip") #if there is 'excess' Y number leave the pin alone and jump to the 'skip' label until we get to the X value
+        jmp(x_not_y, "skip") # if there is 'excess' Y number leave the pin alone and jump to the 'skip' label until we get to the X value
         nop()         .side(1)
         label("skip")
         jmp(y_dec, "loop") #count down y by 1 and jump to pwmloop. When y is 0 we will go back to the 'pull' command
              
-    #simply stops and starts the servo PIO, so the pin could be used for soemthing else.
-    def registerServo(self,servo):
+    # simply stops and starts the servo PIO, so the pin could be used for soemthing else.
+    def register_servo(self, servo: int) -> None:
+        """
+        Registers a servo with the ServoDriver by activating its state machine
+
+        Parameters:
+            servo (int): The index of the servo to register.
+        """
         if(not self.servos[servo].active()):
             self.servos[servo].active(1)
 
-    def deregisterServo(self, servo):
+    def unregister_servo(self, servo: int) -> None:
+        """
+        Unregisters a servo from the ServoDriver by deactivating its state machine
+
+        Parameters:
+            servo (int): The index of the servo to unregister
+        """
         if(self.servos[servo].active()):
             self.servos[servo].active(0)
 
-    def getPosition(self, servo):
-        return self.servoAngles[servo]
+    def get_servo_angle(self, servo: int) -> int:
+        """
+        Returns the current angle of a given servo
 
-    def setPosition(self, servo, degrees):
-        self.__setPosition(servo, degrees)
+        Parameters:
+            servo (int): The index of the servo
+
+        Returns:
+            int: The current angle of the servo
+
+        Raises:
+            UnregisteredServoException: If invalid servo is passed
+        """
+        self.__validate_servo(servo)
+        return self.servo_angles[servo]
+
+    def set_servo_angle(self, servo: int, degrees: int) -> None:
+        """
+        Sets the angle of a given servo
+
+        Parameters:
+            servo (int): The index of the servo
+            degrees (int): The desired angle for the servo
+
+        Raises:
+            UnregisteredServoException: If invalid servo is passed
+            InvalidServoSettingException: If the desired angle exceeds the maximum angle
+        """
+        self.__servo_angle(servo, degrees)
         
-    def setPositionSmooth(self, servo, degrees, delay=0.01, step=1):
-        current_angle = self.getPosition(servo)
+    def set_servo_angle_smooth(self, servo: int, degrees: int, delay: float = 0.01, step: int = 1) -> None:
+        """
+        Smoothly sets the angle of a given servo by moving in small steps
+
+        Parameters:
+            servo (int): The index of the servo
+            degrees (int): The final desired angle for the servo
+            delay (float, optional): The delay between each step. Default is 0.01
+            step (int, optional): The size of each step. Default is 1
+
+        Raises:
+            UnregisteredServoException: If invalid servo is passed
+            InvalidServoSettingException: If the desired angle exceeds the maximum angle
+        """
+        current_angle = self.servo_angles[servo]
         if degrees < current_angle:
             for angle in range(current_angle, degrees - 1, -step):
-                self.__setPosition(servo, angle)
+                self.__servo_angle(servo, angle)
                 sleep(delay)
         else:
             for angle in range(current_angle, degrees + 1, step):
-                self.__setPosition(servo, angle)
+                self.__servo_angle(servo, angle)
                 sleep(delay)
 
-    def __calcPulseLength(self,degrees):
-        return int(degrees*self.degreesToUS + 500)
+    def __calc_pulse_length(self, degrees: int) -> int:
+        """
+        Private method that calculates the pulse length corresponding to a given angle
 
-    # setPosition takes a degree position for the servo to goto. 
-    # 0degrees->180 degrees is 0->2000us, plus offset of 500uS
-    #1 degree ~ 11uS.
-    #This function does the sum then calls goToPeriod to actually poke the PIO 
-    def __setPosition(self,servo, degrees):
-        pulseLength = self.__calcPulseLength(degrees)
-        self.__goToPeriod(servo,pulseLength)
-        self.servoAngles[servo] = degrees
+        Parameters:
+            degrees (int): The angle
+
+        Returns:
+            int: The pulse length corresponding to the angle
+        """
+        return int(degrees * self.DEGREES_TO_PULSE_LEN + self.MIN_SERVO_PULSE)
+
+    # set_servo_angle takes a degree position for the servo to goto. 
+    # 0 degrees->180 degrees is 0->2000us, plus offset of 500uS
+    # 1 degree ~ 11uS.
+    # this function does the sum then calls __write_servo to actually poke the PIO 
+    def __servo_angle(self, servo: int, degrees: int) -> None:
+        """
+        Private method that sets the angle of a given servo and keeps a track of the current angle
+
+        Parameters:
+            servo (int): The index of the servo
+            degrees (int): The desired angle for the servo
+
+        Raises:
+            UnregisteredServoException: If invalid servo is passed
+            InvalidServoSettingException: If the desired angle exceeds the maximum angle
+        """
+        self.__validate_servo(servo)
+        self.__validate_angle(degrees)
+        pulse_length = self.__calc_pulse_length(degrees)
+        self.__write_servo(servo, pulse_length)
+        self.servo_angles[servo] = degrees
     
-    def __goToPeriod(self,servo, period):
-        if(period < 500):
-            period = 500
-        if(period >2500):
-            period =2500
-        #check if servo SM is active, otherwise we are trying to control a thing we do not have control over
-        if self.servos[servo].active():
-            self.servos[servo].put(period)
-        else:
-            raise Exception("TRYING TO CONTROL UNREGISTERED SERVO") #harsh, but at least you'll know
+    def __write_servo(self, servo: int, pulse_length: int) -> None:
+        """
+        Private method that writes the pulse length to a given servo and actually changes the servo angle
 
-    #Class initialisation
-    #defaults to the standard pins and freq for the kitronik board, but could be overridden
-    def __init__(self, numberOfServos = 8, initialAngle = 90):
-        servoPins = [2,3,4,5,6,7,8,9]
+        Parameters:
+            servo (int): The index of the servo
+            pulse_length (int): The pulse length to write to the servo
+
+        """
+        if(pulse_length < self.MIN_SERVO_PULSE):
+            pulse_length = self.MIN_SERVO_PULSE
+        if(pulse_length > self.MAX_SERVO_PULSE):
+            pulse_length = self.MAX_SERVO_PULSE
+        # check if servo SM is active, otherwise we are trying to control a thing we do not have control over
+        if self.servos[servo].active():
+            self.servos[servo].put(pulse_length)
+
+    def __validate_servo(self, servo: int) -> None:
+        """
+        Validates if the given servo is registered
+
+        Parameters:
+            servo (int): The index of the servo
+
+        Raises:
+            UnregisteredServoException: If invalid servo is passed
+        """
+        if(not self.servos[servo].active()):
+            raise UnregisteredServoException(f"Servo is unknown or unregistered: {servo}")
+
+    def __validate_angle(self, degrees: int) -> None:
+        """
+        Validates if the given angle does not exceed the maximum angle
+
+        Parameters:
+            degrees (int): The angle to validate
+
+        Raises:
+            InvalidServoSettingException: If the angle exceeds the maximum angle
+        """
+        if degrees > self.max_angle:
+            raise InvalidServoSettingException(f"Desired angle {degrees} exceeds maximum angle {self.max_angle}.")
+
+    # class initialisation
+    # defaults to the standard pins and freq for the kitronik board, but could be overridden
+    # servo pins on the Simply Servos board are: GP2, GP3, GP4, GP5, GP6, GP7, GP8, GP9 for servos 1-8 in order
+    def __init__(self, servo_pins: List[int] = [2,3,4,5,6,7,8,9], max_angle = 180, initial_angle: int = 90):
+        """
+        Initializes the ServoDriver with a given list of servo pins and sets all servos to an initial angle
+
+        Parameters:
+            servo_pins (List[int], optional): A list of servo pins. Default is [2,3,4,5,6,7,8,9]
+            initial_angle (int, optional): The initial angle for all servos. Default is 90
+        """
+        self.servo_count = len(servo_pins)
+        self.servo_pins = servo_pins
+        self.used_state_machines = [False] * self.servo_count
         self.servos = []
-        self.servoAngles = [0] * numberOfServos  # Initialize empty list for servo angles
-        #connect the servos by default on construction - advanced uses can disconnect them if required.
-        for i in range(numberOfServos):
-            for j in range(8): # StateMachine range from 0 to 7
-                if usedSM[j]:
-                    continue # Ignore this index if already used
+        self.servo_angles = [0] * self.servo_count  # initialize empty list for servo angles
+        self.max_angle = max_angle
+
+        # connect the servos by default on initialization
+        for i in range(self.servo_count):
+            for j in range(self.servo_count): # StateMachine range based on servo_pins
+                if self.used_state_machines[j]:
+                    continue # ignore this index if already used
                 try:
-                    self.servos.append(StateMachine(j, self._servo_pwm, freq=2000000, sideset_base=Pin(servoPins[i])))
-                    usedSM[j] = True # Set this index to used
-                    break # Have claimed the SM, can leave now
+                    self.servos.append(StateMachine(j, self._servo_pwm, freq=2000000, sideset_base=Pin(self.servo_pins[i])))
+                    self.used_state_machines[j] = True # set this index to used
+                    break 
                 except ValueError:
-                    pass # External resouce has SM, move on
-                if j == 7:
-                    # Cannot find an unused SM
+                    pass # external resouce has SM, move on
+                if j == self.servo_count - 1:
+                    # cannot find an unused SM
                     raise ValueError("Could not claim a StateMachine, all in use")
                 
-            self.servos[i].put(self.pulseTrain)
+            self.servos[i].put(self.PULSE_TRAIN)
             self.servos[i].exec("pull()")
             self.servos[i].exec("mov(isr, osr)")
-            #self.servos[i].put(1500)
-            #self.servoAngles[i] = 90
-            self.registerServo(i)
-            self.__setPosition(i,initialAngle)
-            #self.servos[i].put(1500) # Change this between 500 to 2500 for 0 to 180 degrees initial position
-            #self.servoAngles[i] = 90  # Initialize the angle of current servo to 90 degrees
-      
+            self.register_servo(i)
+            self.__servo_angle(i, initial_angle)
